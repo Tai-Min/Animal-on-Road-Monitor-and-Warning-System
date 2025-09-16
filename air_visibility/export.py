f52d6bd351f878f1a3e264dc6e67a040bb1ee5fd
Tai-Min <22417805+Tai-Min@users.noreply.github.com>
2025-09-16 04:31:10 +0200

    Add requirements

--- air_visibility/export.py
+++ air_visibility/export.py
@@ -1,12 +1,10 @@
 import keras
 import os
-import config
 
 if __name__ == "__main__":
     THIS_DIR = os.path.dirname(__file__)
     MODEL_PATH = os.path.join(THIS_DIR, "output/model.keras")
     EXPORT_PATH = os.path.join(THIS_DIR, "../runtime/air_visibility/model/")
-    CLASSES_PATH = os.path.join(EXPORT_PATH, "classes.py")
 
     model = keras.saving.load_model(MODEL_PATH)
     model.export(EXPORT_PATH, format="tf_saved_model")
